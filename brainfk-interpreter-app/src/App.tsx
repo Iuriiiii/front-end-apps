0867ffc3c31601da5f5f074eaf83e9cc6b02e87d
Iuriiiii <metralicnetwork@gmail.com>
2022-09-16 22:00:56 -0300

    +better brainfk

--- brainfk-interpreter-app/src/App.tsx
+++ brainfk-interpreter-app/src/App.tsx
@@ -27,12 +27,13 @@ function doBrainfuck(code: string, input: string = ''): string {
     let output: string[] = [], char: string;
     const chars = code.split('');
     let i = 0;
-    let whileInits: Array<Array<number>> = [];
+    let whileInits: Array<[number, number, boolean]> = [];
     let inputCodeKeys = input.split('').map(c => c.charCodeAt(0));
     let inputLoc = 0;
 
     for (i = 0; i < chars.length; i++) {
         char = chars[i];
+        stack[pointer] ||= 0;
 
         switch (char) {
             case '>':
@@ -42,25 +43,31 @@ function doBrainfuck(code: string, input: string = ''): string {
                 pointer--;
                 break;
             case '+':
-                stack[pointer] = (stack[pointer] || 0) + 1;
-                break;
             case '-':
-                stack[pointer] = (stack[pointer] || 0) - 1;
+                stack[pointer] = (stack[pointer] + (char === '+' ? 1 : 255)) % 256;
                 break;
             case '.':
-                output.push(String.fromCharCode(stack[pointer] || 0));
+                output.push(String.fromCharCode(stack[pointer]));
                 break;
             case ',':
                 stack[pointer] = inputCodeKeys[inputLoc++];
                 break;
             case '[':
-                whileInits.push([i, pointer]);
+                whileInits.push([i, pointer, stack[pointer] === 0] || (whileInits.length > 0 && whileInits.at(-1)![2]));
                 break;
             case ']':
-                if ((stack[pointer] || 0) === 0)
+                if (whileInits.length === 0)
+                    return `Invalid character at ${i}`;
+
+                const whileInit = whileInits.at(-1)!;
+
+                if (whileInit[2] === true)
+                    break;
+
+                if (stack[pointer] === 0)
                     whileInits.pop();
                 else
-                    i = whileInits.at(-1)![0]; //;[whileInits.length-1][0];
+                    i = whileInit[0]; //;[whileInits.length-1][0];
 
                 break;
         }
